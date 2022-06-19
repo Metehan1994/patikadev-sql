c5c2cd832cbaa6f0cafa364ad52e8b31c93342c2
MetehanErdogan <metehan-erdogan@hotmail.com>
2022-06-19 23:11:28 +0300

    Update hw12.sql

--- hw12.sql
+++ hw12.sql
@@ -12,8 +12,8 @@ WHERE rental_rate = (SELECT MIN(rental_rate) FROM film)
 AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
 
 --payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
-SELECT COUNT(payment.customer_id), customer.first_name, customer.last_name FROM payment 
+SELECT SUM(amount), customer.first_name, customer.last_name FROM payment 
 INNER JOIN customer ON customer.customer_id = payment.customer_id 
 GROUP BY payment.customer_id, customer.first_name, customer.last_name 
-ORDER BY COUNT(payment.customer_id) DESC 
+ORDER BY SUM(amount) DESC 
 LIMIT 10
